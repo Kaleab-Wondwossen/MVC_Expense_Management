68921717ffdf65be6d7e5219f809c37e225e93fd
kaleab2114 <103066215+kaleab2114@users.noreply.github.com>
2023-12-17 21:48:05 +0300

    we have added to the content of the README file

--- app/core/app.php
+++ app/core/app.php
@@ -1,15 +1,23 @@
-<?php
-class App{
+class App {
     protected $controller = 'home';
     protected $method = 'index';
     protected $params = [];
-    public function __construct(){
-        $this->parseURL();
+
+    public function __construct() {
+        $url = $this->parseURL();
+
+        if (file_exists('../app/controllers/'. $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/'. $this->controller . '.php';
     }
-    public function parseURL(){
+
+    public function parseURL() {
         if (isset($_GET['url'])) {
-            echo $_GET['url'];
+            $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+            return $url;
         }
     }
 }
-?>
